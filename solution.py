fc2b5c47807a6dcbe63f51d433cabd9c36e340ea
Nathan Vulakh <nathan@nathans-mbp.lan>
2021-09-05 18:53:55 -0400

    fix question text

--- solution.py
+++ solution.py
@@ -7,24 +7,18 @@ def welcome_assignment_answers(question):
     elif question == "In Slack, what is the secret passphrase posted in the " \
                      "#cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"
-    elif question == "Is it possible to decode a message without a key?":
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
-    elif question == "Is a hashed message supposed to be un-hashed?":
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking'":
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' " \
+                     "- Use MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
-    elif question == "Is MD5 a secured hashing algorithm?":
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "Yes"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to?":
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 3
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to?":
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 2
     return(answer)
 # Complete all the questions.
-
-
-
-if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
